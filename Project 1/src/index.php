20c4936a9ba72945ce518faeeed92d61eeaf87ce
Vipul Walia <vipertecpro@gmail.com>
2017-06-16 17:33:39 +0530

    Project Setup

---
+++ Project 1/src/index.php
@@ -0,0 +1,54 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="res/img/favicon.png">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+	<title>ALPHA</title>
+    
+    <!-- CSS  -->
+    <link href="res/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>    
+    <link href="res/css/material.cyan-light_blue.min.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="res/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="res/css/root.css" type="text/css" rel="stylesheet" media="screen,projection" />
+</head>
+<body>
+
+  
+    <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+      <header class="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">Home</span>
+        </div>
+      </header>
+      <div class="demo-drawer mdl-layout__drawer mdl-color--blue-grey-900 mdl-color-text--blue-grey-50">
+        <header class="demo-drawer-header">
+          <img src="res/img/user.jpg" class="demo-avatar">
+        </header>
+        <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i>Home</a>
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">inbox</i>Inbox</a>
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">delete</i>Trash</a>
+          <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">report</i>Spam</a>
+        </nav>
+      </div>
+      <main class="mdl-layout__content mdl-color--grey-100">
+        <div class="mdl-grid demo-content">
+        <h1>HELLo</h1>
+        </div>
+      </main>
+    </div>
+    
+    
+    <!--  Scripts-->
+    <script src="res/js/jquery-3.2.1.min.js"></script>
+    <script src="res/js/materialize.min.js"></script>
+    <script src="res/js/material.min.js"></script>
+    <script src="res/js/init.js"></script>
+    <script src="res/js/root.js"></script>    
+
+</body>
+</html>
